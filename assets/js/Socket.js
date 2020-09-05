2c32fe26032bcd4dff07671a2c8b33f5584a85d6
Erl777 <erl.pank777@gmail.com>
2020-09-05 17:28:24 +0300

    рабочее соединение веб сервера

--- assets/js/Socket.js
+++ assets/js/Socket.js
@@ -7,7 +7,7 @@ class Socket {
     }
 }
 
-let socket = new WebSocket("ws://js.info");
+let socket = new WebSocket("ws://localhost:3001");
 
 socket.onopen = function(e) {
     alert("Соединение установлено");
